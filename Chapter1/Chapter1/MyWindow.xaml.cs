a2b0a953dd16e3009c20ddd0239a2d2051092318
YU <ankoanko3.14@gmail.com>
2024-02-24 11:36:46 +0900

    [p30]リソースバインディング

--- Chapter1/Chapter1/MyWindow.xaml.cs
+++ Chapter1/Chapter1/MyWindow.xaml.cs
@@ -1,23 +1,20 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Chapter1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MyWindow : Window
     {
-        public MainWindow()
+        public MyWindow()
         {
+            InitializeComponent();
+        }
+
+        void HowdyClicked(object sender, RoutedEventArgs e) 
+        {
+            _text1.Text = "C#からこんにちは！";
         }
     }
 }
